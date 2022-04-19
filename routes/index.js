9b867985c00256ee69f63bec583a3604aaedafae
Greig Corbett-Smith <gsmith.and.company@gmail.com>
2022-04-20 10:28:10 +1200

    Added DB connection

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql')
+
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'bethel123',
+  database: 'goodnews'
+});
+
+connection.connect(function(err){
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log('Nice! You are connected to the DB!')
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
