d3859599927dd5d627219705ff4ee0af01655df2
Dustin Kelley <kelleydustin2018@gmail.com>
2022-04-21 18:55:55 -0500

    added user routes and some auth

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const { User } = require('../models');
+var bcrypt = require('bcrypt');
+const async = require('hbs/lib/async');
 
 /* GET users listing for ADMIN? TBC */
 router.get('/', (req, res, next) => {
@@ -10,26 +12,30 @@ router.get('/', (req, res, next) => {
 });
 
 //Sign Up
-router.post('/', async function(req, res, next) {
+router.post('/', async function(req, res, next) { // is async correct syntax?
 
   if(!req.body.user_name || !req.body.password ) {
     res.status(400).send('Username and Password required');
     return;
   }
+    //hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
 
   User.create({
       //admin: req.body.admin,  Does this need to be here?
       first_name: req.body.first_name,
       last_name: req.body.last_name,
       user_name: req.body.user_name, 
-      password: req.body.password,
+      password: hashedPassword,
       email: req.body.email,
       address: req.body.address,
       state: req.body.state,
       city: req.body.city,
       zip_code: req.body.zip_code,
       country: req.body.country,
-      UserId: user.id //Is this the correct id?
+      UserId: user.id 
   }).then(newUser => {
       res.json({
         id: newUser.id,
@@ -41,6 +47,25 @@ router.post('/', async function(req, res, next) {
 });
 
 //Login
+router.post('/login', async function(req, res, next) {
+  User.findOne({
+    where: {
+      user_name: req.body.user_name
+    }
+  }).then(async user =>{
+    if (!user) {
+      res.status(404).send('Invalid Username');
+      return;
+    }
+    const valid = await bcrypt.compare(req.body.password, user.password);
+
+    if (valid) {
+      res.status(200).send('Hi!' + user.user_name);
+    } else {
+      res.status(401).send("Invalid Password");
+    }
+  });
+});
 
 
 module.exports = router;
