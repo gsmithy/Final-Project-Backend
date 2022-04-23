da9db80bd7a9a03567866d25b6248d1e3c5c4c6c
Greig Corbett-Smith <gsmith.and.company@gmail.com>
2022-04-23 16:42:30 +1200

    added CORS to app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var models = require('./models');
+var cors = require('cors');
 
 
 var usersRouter = require('./routes/users');
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 //Sync DB to models
 models.sequelize.sync({ alter:true }).then(function(){
