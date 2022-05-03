5b0ca9fdf20cd53672996c299ca1a44689449fee
Greig Corbett-Smith <gsmith.and.company@gmail.com>
2022-05-03 20:08:27 +1200

    added routes for likes and loves.

--- services/auth.js
+++ services/auth.js
@@ -4,7 +4,6 @@ const { User } = require('../models');
 
 const secretKey = 'iamthesecretkey';
 
-/* JWT Token creation */
 module.exports = {
     createJWT: (user) => {
         const token = jwt.sign({
