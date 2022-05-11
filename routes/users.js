20987139a5bd71f950d43f30b669c2860cb3f3aa
Greig Corbett-Smith <gsmith.and.company@gmail.com>
2022-05-12 11:31:40 +1200

    reordered a couple things

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const auth = require('../services/auth');
-const { User } = require('../models');
-const cors = require("cors")
+const bcrypt = require("bcrypt");
+const auth = require("../services/auth");
+const { User } = require("../models");
+const cors = require("cors");
 
 router.use(cors());
 
-/* POST USER LOGIN */ 
-router.post('/login', async (req, res, next) => {
-
-
-
+/* POST USER LOGIN */
+router.post("/login", async (req, res, next) => {
   User.findOne({
     where: {
-      user_name: req.body.user_name
-    }
-  }).then( async user => {
+      user_name: req.body.user_name,
+    },
+  }).then(async (user) => {
     if (!user) {
-      res.status(404).send('Invalid Username');
+      res.status(404).send("Invalid Username");
       return;
-    };
+    }
     const valid = await bcrypt.compare(req.body.password, user.password);
 
     if (valid) {
       const jwt = auth.createJWT(user);
-      res.status(201).send({ jwt }); 
+      res.status(201).send({ jwt });
     } else {
       res.status(401).send("Invalid Password!");
     }
@@ -33,34 +30,29 @@ router.post('/login', async (req, res, next) => {
 });
 
 /* POST USER SIGN UP */
-//NEEDS PIMPING!! WAS TRYING TO ADD A CHECK TO SEE THAT THE USER DOESNT ALREADY EXIST VIA
-//EMAIL. THEN ANOTHER CHECK TO MAKE SURE THEY'RE USING A UNIQUE USERNAME AS A 400 WILL OCCUR APON
-//THAT HAPPENING WITHOUT ANY NOTIFICATION TO THE USER. THEN CREATE USER. 
-router.post('/signup', (req, res, next) => { 
-
-res.setHeader("Acess-Control-Allow-Origin", "http://localhost:3000");
+router.post("/signup", (req, res, next) => {
+  res.setHeader("Acess-Control-Allow-Origin", "http://localhost:3000");
 
   console.log("////Its me!");
-  if(!req.body.user_name || !req.body.password ) {
+  if (!req.body.user_name || !req.body.password) {
     console.log(req.body.user_name);
-    res.status(400).send('Username and Password required');
+    res.status(400).send("Username and Password required");
     return;
-  };
-  if(req.body.user_name === User.user_name){
-    console.log(req.body.user_name + 'line41');
-    res.status(400).send('message: Sorry..Username already exists!' );
+  }
+  if (req.body.user_name === User.user_name) {
+    console.log(req.body.user_name + "line41");
+    res.status(400).send("message: Sorry..Username already exists!");
     return;
-  };
+  }
 
   //const salt = await bcrypt.genSalt(10);
   //const hashedPassword = await bcrypt.hash(req.body.password, salt);
-//console.log('now creating user');
+  //console.log('now creating user');
   User.create({
-    
     admin: req.body.admin,
     first_name: req.body.first_name,
     last_name: req.body.last_name,
-    user_name: req.body.user_name, 
+    user_name: req.body.user_name,
     password: auth.hashPassword(req.body.password),
     email: req.body.email,
     address: req.body.address,
@@ -68,90 +60,47 @@ res.setHeader("Acess-Control-Allow-Origin", "http://localhost:3000");
     city: req.body.city,
     zip_code: req.body.zip_code,
     country: req.body.country,
-    profile_pic: req.body.profile_pic
-     
-
-}).then(newUser => {
-  console.log(newUser);
-    res.status(201).json({
-            createdUser: newUser,
-            status: 200,
-            message: 'user successfully created!'
-            // id: newUser.id,
-            // user_name: newUser.user_name
-
-            })
-}).catch((err) => {
-    res.status(400).send("Sorry, the user you're trying to create already exists!");
-    console.log(err)
-});
-});
-
-/* USER GETS - User views profile page & info */
-  router.get('/:username', async (req, res, next) => {
-    const userProfile = req.params.username;
-    
-    // const user = req.user;
-    //     if (!user){
-    //         res.status(403).send('Please log in!');
-    //         return;
-    //     };
-
-    User.find({
-        where: {
-            user_name: userProfile
-        }
-    }).then( user => {
-        if (user){
-            res.status(200).send({
-
-              first_name: user.first_name,
-              last_name: user.last_name,
-              user_name: user.user_name,
-              email: user.email,
-              address: user.address,
-              state: user.state,
-              city: user.city,
-              zip_code: user.zip_code,
-              country: user.country,
-              profile_pic: user.profile_pic
-              
-            });
-        } else {
-            res.status(400).send('Oops! Something went wrong!')
-        }
+    profile_pic: req.body.profile_pic,
+  })
+    .then((newUser) => {
+      console.log(newUser);
+      res.status(201).json({
+        createdUser: newUser,
+        status: 200,
+        message: "user successfully created!",
+        // id: newUser.id,
+        // user_name: newUser.user_name
+      });
     })
+    .catch((err) => {
+      res
+        .status(400)
+        .send("Sorry, the user you're trying to create already exists!");
+      console.log(err);
+    });
 });
 
-
-
-router.post("/getOne", (req, res) => {
+/* POST USER PROFILE - User views own info */
+router.post("/getInfo", (req, res) => {
   res.setHeader("Acess-Control-Allow-Origin", "http://localhost:3000");
 
-
-
-let token = req.body.jwt
-
+  let token = req.body.jwt;
 
   if (token) {
-    auth.verifyUser(token).then(user => {
+    auth.verifyUser(token).then((user) => {
       if (user) {
-        console.log(user)
+        // console.log(user);
         User.findOne({
           where: {
-           id: user.id
-          }
-        }).then(response => {
-          res.json(response)
-        })
+            id: user.id,
+          },
+        }).then((response) => {
+          // console.log(response)
+          res.json(response);
+        });
       }
-    })
+    });
   }
-
-
-
-
-})
-
+});
 
 module.exports = router;
