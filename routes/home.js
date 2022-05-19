05aa401b01cd72237b18b83ea06c6ba445351f03
Conor Wiley <wileyconro98@gmail.com>
2022-05-19 12:43:11 -0700

    added some querys for home

--- routes/home.js
+++ routes/home.js
@@ -5,12 +5,28 @@ const { Post } = require('../models');
 /* GET HOME PAGE - returns all posts. */
 router.get('/home', async (req, res, next) => {
   
-  Post.findAll()
+  Post.findAll({
+    limit: 3,
+    order: [ [ 'createdAt', 'DESC' ]]
+  })
     .then( postList => {
         res.status(200).send(postList);
     }).catch( err => {
         res.status(404).send(err);
     }) 
 });
+
+router.get('/homelist', async (req, res, next) => {
+  
+    Post.findAll({
+      limit: 10,
+      order: [ [ 'createdAt', 'DESC' ]]
+    })
+      .then( postList => {
+          res.status(200).send(postList);
+      }).catch( err => {
+          res.status(404).send(err);
+      }) 
+  });
 
 module.exports = router;
